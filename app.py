fd806064d69536aef75e76b45dd56da98015c802
jianyang <jyang95@hotmail.com>
2023-06-04 13:42:42 +0800

    Second commit

--- app.py
+++ app.py
@@ -1,37 +1,39 @@
-from flask import Flask, render_template, request
-from flask_ngrok import run_with_ngrok
+from flask import Flask, request, render_template
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
-import torch
+from pyngrok import ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)
 
-default_model_name = 'gpt2'
-default_model = GPT2LMHeadModel.from_pretrained(default_model_name)
-default_tokenizer = GPT2Tokenizer.from_pretrained(default_model_name)
-
-fine_tuned_model_name = 'gpt2'
-fine_tuned_model = GPT2LMHeadModel.from_pretrained(fine_tuned_model_name)
-fine_tuned_tokenizer = GPT2Tokenizer.from_pretrained(fine_tuned_model_name)
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+default_model = GPT2LMHeadModel.from_pretrained('gpt2')
+fine_tuned_model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    user_input = request.form['user_input']
+    message = request.form['message']
+    
+    default_response = generate_response(default_model, message)
+    fine_tuned_response = generate_response(fine_tuned_model, message)
+    
+    response = {
+        'default': default_response,
+        'fine_tuned': fine_tuned_response
+    }
+    
+    return response
 
-    input_ids = default_tokenizer.encode(user_input, return_tensors='pt')
-    default_output = default_model.generate(input_ids, max_length=100, num_return_sequences=1)
-    default_response = default_tokenizer.decode(default_output[0], skip_special_tokens=True)
-
-    input_ids = fine_tuned_tokenizer.encode(user_input, return_tensors='pt')
-    fine_tuned_output = fine_tuned_model.generate(input_ids, max_length=100, num_return_sequences=1)
-    fine_tuned_response = fine_tuned_tokenizer.decode(fine_tuned_output[0], skip_special_tokens=True)
-
-    return render_template('index.html', user_input=user_input, default_response=default_response,
-                           fine_tuned_response=fine_tuned_response)
+def generate_response(model, message):
+    input_ids = tokenizer.encode(message, return_tensors='pt')
+    output = model.generate(input_ids, max_length=50, num_return_sequences=1)
+    response = tokenizer.decode(output[0], skip_special_tokens=True)
+    
+    return response
 
 if __name__ == '__main__':
     app.run()
+    public_url = ngrok.connect(5000).public_url
+    print("Public URL:", public_url)
